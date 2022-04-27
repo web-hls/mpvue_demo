b3fe06c4818d111e12da34bfdd6acf54cb7748a0
黄丽珊 <huanglishan@qdama.cn>
2022-04-27 13:12:17 +0800

    add_pictrue

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,43 @@
 <template>
   <div>
-    home页面
+    
+    <div :class="[isActive ? 'htmlActive' : '', 'errorClass']"> html绑定class </div>
+    <div :class="computedClassObject"> js绑定class </div>
+
+    <div :style="{ color : isActive ? 'green' : '' }"> html绑定style </div>
+    <div :style="computedStyleObject"> style绑定style </div>
+
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      isActive: true
+    }
+  },
+  computed: {
+    computedClassObject() {
+      return [ this.isActive ? 'jsActive' : '', 'errorClass' ]
+    },
+    computedStyleObject() {
+      return { color : this.isActive ? 'yellow' : '' }
+    }
   }
 }
 
 </script>
 
 <style scoped>
-
+.htmlActive {
+  color:crimson
+}
+.jsActive {
+  color: orange
+}
+.errorClass {
+  font-weight: 700;
+  font-size: 30px;
+}
 </style>
